6235fc1ad3370600c9fe2f209ab65b20f63d4c68
gmzyy <angeljsjsjbdd@gmail.com>
2025-07-06 02:55:58 -0600

    db corregida

--- config/db.js
+++ config/db.js
@@ -9,7 +9,14 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
-
+console.log("🔍 ENV:", {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+  DB_SSL: process.env.DB_SSL
+});
 db.connect((err) => {
   if (err) {
     console.error('❌ Error de conexión a MySQL:', err.message);
