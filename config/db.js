93c56621c52bbc299bd194519f4416ffdc45144f
gmzyy <angeljsjsjbdd@gmail.com>
2025-07-06 03:03:10 -0600

    correcion de usuario

--- config/db.js
+++ config/db.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,        
-  port: process.env.DB_PORT,        
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,              
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
+
 console.log("🔍 ENV:", {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
@@ -17,6 +19,7 @@ console.log("🔍 ENV:", {
   DB_PORT: process.env.DB_PORT,
   DB_SSL: process.env.DB_SSL
 });
+
 db.connect((err) => {
   if (err) {
     console.error('❌ Error de conexión a MySQL:', err.message);
